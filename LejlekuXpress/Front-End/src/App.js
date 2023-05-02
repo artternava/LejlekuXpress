4bf551d262cbf17284c160f351bd8b2845eb30c4
ArditBeqaj <arditi.b75@gmail.com>
2023-05-02 23:40:02 +0200

    Adding BreadCrumbs and Meta Components.

--- LejlekuXpress/Front-End/src/App.js
+++ LejlekuXpress/Front-End/src/App.js
@@ -5,6 +5,7 @@ import Layout from './components/Layout';
 import Home from './pages/Home';
 import About from './pages/About';
 import Contact from './pages/Contact';
+import OurStore from './pages/OurStore';
 
 function App() {
   return (
@@ -14,6 +15,7 @@ function App() {
           <Route path='/' element={<Layout />}>
             <Route index element={<Home />}/>
             <Route path='about' element={<About />}/>
+            <Route path='store' element={<OurStore />}/>
             <Route path='contact' element={<Contact />}/>
           </Route>
         </Routes>
---
+++ LejlekuXpress/Front-End/src/pages/OurStore.js
@@ -0,0 +1,14 @@
+import React from 'react'
+import BreadCrumb from '../components/BreadCrumb'
+import Meta from '../components/Meta'
+
+const OurStore = () => {
+  return (
+    <>
+    <Meta title="Our Store"></Meta>
+    <BreadCrumb title = "Our Store"></BreadCrumb>
+    </>
+  )
+}
+
+export default OurStore
