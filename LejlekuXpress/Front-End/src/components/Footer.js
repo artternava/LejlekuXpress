68e6648a587d96214d4106f838959a1332880c49
DrenKastratii <121746111+DrenKastratii@users.noreply.github.com>
2023-05-02 14:06:58 +0200

    Creating the dropdown product categories and footer.

--- LejlekuXpress/Front-End/src/components/Footer.js
+++ LejlekuXpress/Front-End/src/components/Footer.js
@@ -2,8 +2,20 @@ import React from 'react'
 
 const Footer = () => {
   return (
-    <div>Footer</div>
-  )
+    <>
+      <footer className='py-4'>
+        <div className="container-xl">
+          <div className="row">
+            <div className="col-12">
+              <p className='text-center mb-0 text-white'>
+                &copy; {new Date().getFullYear()}: LejlekuXPress. All rights reserved.
+              </p>
+            </div>
+          </div>
+        </div>
+      </footer>
+    </>
+  );
 }
 
 export default Footer;
--- LejlekuXpress/Front-End/src/components/Header.js
+++ LejlekuXpress/Front-End/src/components/Header.js
@@ -1,12 +1,14 @@
 import React from "react";
 import { NavLink, Link } from "react-router-dom";
-import logo from "./logo.ico";
 import { BsSearch } from "react-icons/bs";
+
+
 
 const Header = () => {
   return (
     <>
-      <header className="header-upper">
+      {/* Navbari Kryesor */}
+      <header className="header-upper py-3">
         <div className="container-xxl px-8">
           <div className="row align-items-center">
             <div className="col-3">
@@ -41,22 +43,79 @@ const Header = () => {
                   </Link>
                 </div>
                 <div>
-                <Link className="d-flex align-items-center gap-10 text-white">
+                  <Link className="d-flex align-items-center gap-10 text-white">
                     <img src="images/cart.svg" alt="cart" />
                     <div className="d-flex flex-column gap-10">
                       <span className="badge bg-white text-dark"></span>
                       <p className="mb-0">$ 0.00</p>
                     </div>
                   </Link>
-
                 </div>
                 <div>
-                <Link className="d-flex align-items-center gap-10 text-white">
+                  <Link className="d-flex align-items-center gap-10 text-white">
                     <img src="images/user.svg" alt="user" />
                     <p className="mb-0">
                       Login <br /> My Account
                     </p>
                   </Link>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+      </header>
+      {/*Dropdown categories*/}
+      <header className="header-bottom py-2">
+        <div className="container-xxl">
+          <div className="row">
+            <div className="col-12">
+              <div className="menu-bottom d-flex align-items-center gap-30">
+                <div>
+                  <div className="dropdown">
+                    <button
+                      class="btn btn-secondary dropdown-toggle bg-transparent border-0 gap-10 m d-flex align-items-center"
+                      type="button"
+                      data-bs-toggle="dropdown"
+                      aria-expanded="false"
+                    >
+                      <img src="images/menu.svg" alt="" />
+                      <span className="me-5 d-inline-block">Shop Categories</span>
+                    </button>
+                    <ul className="dropdown-menu">
+                      <li>
+                        <Link className="dropdown-item text-white" to="">
+                          Clothes
+                        </Link>
+                      </li>
+                      <li>
+                        <Link className="dropdown-item text-white" to="">
+                          Cosmetics
+                        </Link>
+                      </li>
+                      <li>
+                        <Link className="dropdown-item text-white" to=" text-white">
+                          Accesories
+                        </Link>
+                      </li>
+                      <li>
+                        <Link className="dropdown-item text-white" to=" text-white">
+                          Technology
+                        </Link>
+                      </li>
+                      <li>
+                        <Link className="dropdown-item text-white" to=" text-white">
+                          Sport
+                        </Link>
+                      </li>
+                    </ul>
+                  </div>
+                </div>
+                <div className="menu-links">
+                  <div className="d-flex align-items-center gap-15">
+                    <NavLink to="/">Home</NavLink>
+                    <NavLink to="/">Our Store</NavLink>
+                    <NavLink to="contact">Contacts</NavLink>
+                  </div>
                 </div>
               </div>
             </div>
