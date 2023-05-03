e86ebaca1c487bc24e3e0ba48818d85904992380
DrenKastratii <121746111+DrenKastratii@users.noreply.github.com>
2023-05-03 17:06:52 +0200

    Adding the Special Product Section to our HomePage.

--- LejlekuXpress/Front-End/src/pages/Home.js
+++ LejlekuXpress/Front-End/src/pages/Home.js
@@ -1,15 +1,16 @@
 import React from "react";
 import { Link } from "react-router-dom";
 import Marquee from "react-fast-marquee";
-import {Helmet} from "react-helmet";
+import { Helmet } from "react-helmet";
+import SpecialProduct from "../components/SpecialProduct";
 
 const Home = () => {
   return (
     <>
       <Helmet>
-         <meta charSet="utf-8" />
-         <title>Home</title>
-         <link rel="canonical" href="http://mysite.com/example" />
+        <meta charSet="utf-8" />
+        <title>Home</title>
+        <link rel="canonical" href="http://mysite.com/example" />
       </Helmet>
       {/* pjesa e pare e HomePage */}
       <section className="home-wrapper-1 py-5">
@@ -25,7 +26,9 @@ const Home = () => {
                 <div className="main-banner-content position-absolute">
                   <h5>SUPERCHARGED FOR PROS.</h5>
                   <h6>IPad s13+ Pro.</h6>
-                  <p>From 999€ or 41.62€/mo. <br /> for 24 mo.</p>
+                  <p>
+                    From 999€ or 41.62€/mo. <br /> for 24 mo.
+                  </p>
                   <Link className="button">BUY NOW</Link>
                 </div>
               </div>
@@ -41,7 +44,9 @@ const Home = () => {
                   <div className="small-banner-content top-banner-pictures">
                     <h5>BEST SALE</h5>
                     <h6>Laptops Max.</h6>
-                    <p>From 999€ or <br /> 41.62€/mo.</p>
+                    <p>
+                      From 999€ or <br /> 41.62€/mo.
+                    </p>
                   </div>
                 </div>
                 <div className="small-banner positon-relative ">
@@ -53,7 +58,9 @@ const Home = () => {
                   <div className="small-banner-content top-banner-pictures">
                     <h5>NEW ARRIVAL</h5>
                     <h6>Buy IPad Air</h6>
-                    <p>From 599€ or <br /> 41.62€/mo. for 12 mo.</p>
+                    <p>
+                      From 599€ or <br /> 41.62€/mo. for 12 mo.
+                    </p>
                   </div>
                 </div>
 
@@ -66,7 +73,9 @@ const Home = () => {
                   <div className="small-banner-content bottom-banner-pictures">
                     <h5>15% OFF</h5>
                     <h6>Smartwatch 7</h6>
-                    <p>Shop the latest band <br /> styles and colors</p>
+                    <p>
+                      Shop the latest band <br /> styles and colors
+                    </p>
                   </div>
                 </div>
                 <div className="small-banner positon-relative ">
@@ -78,7 +87,9 @@ const Home = () => {
                   <div className="small-banner-content bottom-banner-pictures">
                     <h5>FREE ENGRAVING</h5>
                     <h6>AirPods Max</h6>
-                    <p>High-fidelity playback & <br /> ultra-low distortion</p>
+                    <p>
+                      High-fidelity playback & <br /> ultra-low distortion
+                    </p>
                   </div>
                 </div>
               </div>
@@ -86,7 +97,6 @@ const Home = () => {
           </div>
         </div>
       </section>
-
 
       {/* pjesa e dyte e HomePage */}
       <section className="home-wrapper-2 py-5">
@@ -141,56 +151,56 @@ const Home = () => {
             <div className="col-12">
               <div className="categories d-flex justify-content-between flex-wrap align-items-center">
                 <div className="d-flex gap-15 align-items-center">
-                  <div >
+                  <div>
                     <h6>Music</h6>
                     <p>10 items</p>
                   </div>
                   <img src="images/speaker.jpg" alt="speaker" />
                 </div>
                 <div className="d-flex gap-15 align-items-center">
-                  <div >
+                  <div>
                     <h6>Cameras</h6>
                     <p>10 items</p>
                   </div>
                   <img src="images/camera.jpg" alt="camera" />
                 </div>
                 <div className="d-flex gap-15 align-items-center">
-                  <div >
+                  <div>
                     <h6>Smart TV</h6>
                     <p>10 items</p>
                   </div>
                   <img src="images/tv.jpg" alt="tv" />
                 </div>
                 <div className="d-flex gap-15 align-items-center">
-                  <div >
+                  <div>
                     <h6>HeadPhones</h6>
                     <p>10 items</p>
                   </div>
                   <img src="images/headphone.jpg" alt="headphone" />
                 </div>
                 <div className="d-flex gap-15 align-items-center">
-                  <div >
+                  <div>
                     <h6>Music</h6>
                     <p>10 items</p>
                   </div>
                   <img src="images/speaker.jpg" alt="speaker" />
                 </div>
                 <div className="d-flex gap-15 align-items-center">
-                  <div >
+                  <div>
                     <h6>Cameras</h6>
                     <p>10 items</p>
                   </div>
                   <img src="images/camera.jpg" alt="camera" />
                 </div>
                 <div className="d-flex gap-15 align-items-center">
-                  <div >
+                  <div>
                     <h6>Smart TV</h6>
                     <p>10 items</p>
                   </div>
                   <img src="images/tv.jpg" alt="tv" />
                 </div>
                 <div className="d-flex gap-15 align-items-center">
-                  <div >
+                  <div>
                     <h6>HeadPhones</h6>
                     <p>10 items</p>
                   </div>
@@ -200,9 +210,24 @@ const Home = () => {
             </div>
           </div>
         </div>
-      
       </section>
-
+      <section className="special-wrapper py-5 home-wrapper-2">
+        <div className="container-xxl">
+          <div className="row">
+            <div className="col-12">
+              <h3 className="section-heading">Special Products</h3>
+            </div>
+          </div>
+          <div className="row">
+            <SpecialProduct />
+            <SpecialProduct />
+            <SpecialProduct />
+            <SpecialProduct />
+            <SpecialProduct />
+            <SpecialProduct />
+          </div>
+        </div>
+      </section>
       <section className="marquee-wrapper py-5">
         <div className="container xxl">
           <div className="row">
