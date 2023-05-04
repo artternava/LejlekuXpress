347afa890d21d1463754ed8ac6179c2b5c1a42af
ArditBeqaj <arditi.b75@gmail.com>
2023-05-04 18:22:31 +0200

    Adding the products in Our Store page.

--- LejlekuXpress/Front-End/src/components/ProductCard.js
+++ LejlekuXpress/Front-End/src/components/ProductCard.js
@@ -11,19 +11,19 @@ const ProductCard = () => {
             </Link>
         </div>
         <div className='product-image'>
-            <img src='images/watch.jpg' className='img-fluid' alt='product img'></img>
-            <img src='images/hoverimage.jpg'className='img-fluid'  alt='product img'></img>
+            <img src='images/tab.jpg' className='img-fluid' alt='product img'></img>
+            <img src='images/tab1.jpg'className='img-fluid'  alt='product img'></img>
         </div>
         <div className='product-details'>
             <h6 className='brand'>Havels</h6>
             <h5 className='product-title'>
-                Kids headphones bulk 10 pack multi color
+                Samsung tablet 
             </h5>
             <ReactStars count={5} size={24} value="3" edit={false} activeColor="#ffd700"/>
             <p className='price'>$100.00</p>
         </div>
         <div className='action-bar position-absolute'>
-            <div className='d-flex flex-column gap-15'>
+            <div className='d-flex flex-column gap-10'>
                 <Link>
                     <img src='images/view.svg' alt='view'></img>
                 </Link>
