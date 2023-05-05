f232fd6d05d8d2221d591ae8fe10f7afb9c65a81
DrenKastratii <121746111+DrenKastratii@users.noreply.github.com>
2023-05-05 15:40:38 +0200

    path warning fixed

--- LejlekuXpress/Front-End/src/pages/SingleProduct.js
+++ LejlekuXpress/Front-End/src/pages/SingleProduct.js
@@ -2,19 +2,18 @@ import React from "react";
 import { useState } from "react";
 import ReactStars from "react-stars";
 import ReactImageZoom from "react-image-zoom";
-import { AiOutlineHeart } from 'react-icons/ai';
+import { AiOutlineHeart } from "react-icons/ai";
 
 const SingleProduct = () => {
   const props = {
     width: 400,
     height: 500,
     zoomWidth: 600,
-    img: "https://images.pexels.com/photos/190819/pexels-photo-190819.jpeg?cs=srgb&dl=pexels-fernando-arcos-190819.jpg&fm=jpg"
+    img: "https://images.pexels.com/photos/190819/pexels-photo-190819.jpeg?cs=srgb&dl=pexels-fernando-arcos-190819.jpg&fm=jpg",
   };
-  const [orderedProduct, setorderProduct] = useState(true);
+  const [orderedProduct] = useState(true);
   return (
     <>
-
       <div className="main-product-wrapper py-5 home-wrapper-2">
         <div className="container-xxl">
           <div className="row">
@@ -25,10 +24,34 @@ const SingleProduct = () => {
                 </div>
               </div>
               <div className="other-product-images d-flex flex-wrap gap-15">
-                <div><img src="https://images.pexels.com/photos/190819/pexels-photo-190819.jpeg?cs=srgb&dl=pexels-fernando-arcos-190819.jpg&fm=jpg" className="img-fluid" alt="" /></div>
-                <div><img src="https://images.pexels.com/photos/190819/pexels-photo-190819.jpeg?cs=srgb&dl=pexels-fernando-arcos-190819.jpg&fm=jpg" className="img-fluid" alt="" /></div>
-                <div><img src="https://images.pexels.com/photos/190819/pexels-photo-190819.jpeg?cs=srgb&dl=pexels-fernando-arcos-190819.jpg&fm=jpg" className="img-fluid" alt="" /></div>
-                <div><img src="https://images.pexels.com/photos/190819/pexels-photo-190819.jpeg?cs=srgb&dl=pexels-fernando-arcos-190819.jpg&fm=jpg" className="img-fluid" alt="" /></div>
+                <div>
+                  <img
+                    src="https://images.pexels.com/photos/190819/pexels-photo-190819.jpeg?cs=srgb&dl=pexels-fernando-arcos-190819.jpg&fm=jpg"
+                    className="img-fluid"
+                    alt=""
+                  />
+                </div>
+                <div>
+                  <img
+                    src="https://images.pexels.com/photos/190819/pexels-photo-190819.jpeg?cs=srgb&dl=pexels-fernando-arcos-190819.jpg&fm=jpg"
+                    className="img-fluid"
+                    alt=""
+                  />
+                </div>
+                <div>
+                  <img
+                    src="https://images.pexels.com/photos/190819/pexels-photo-190819.jpeg?cs=srgb&dl=pexels-fernando-arcos-190819.jpg&fm=jpg"
+                    className="img-fluid"
+                    alt=""
+                  />
+                </div>
+                <div>
+                  <img
+                    src="https://images.pexels.com/photos/190819/pexels-photo-190819.jpeg?cs=srgb&dl=pexels-fernando-arcos-190819.jpg&fm=jpg"
+                    className="img-fluid"
+                    alt=""
+                  />
+                </div>
               </div>
             </div>
             <div className="col-6">
@@ -48,7 +71,9 @@ const SingleProduct = () => {
                     />
                     <p className="mb-0 t-review">(2 reviews)</p>
                   </div>
-                  <a className='review-btn' href="#review">Write a review</a>
+                  <a className="review-btn" href="#review">
+                    Write a review
+                  </a>
                 </div>
                 <div className="border-bottom py-3">
                   <div className="d-flex gap-10 align-items-center my-2">
@@ -74,30 +99,47 @@ const SingleProduct = () => {
                   <div className="d-flex gap-10 flex-column mt-2 mb-3">
                     <h3 className="product-heading">Size :</h3>
                     <div className="d-flex flex-wrap gap-15">
-                      <span className="badge border border-1 bg-white text-dark border-secondary">S</span>
-                      <span className="badge border border-1 bg-white text-dark border-secondary">M</span>
-                      <span className="badge border border-1 bg-white text-dark border-secondary">L</span>
-                      <span className="badge border border-1 bg-white text-dark border-secondary">XL</span>
+                      <span className="badge border border-1 bg-white text-dark border-secondary">
+                        S
+                      </span>
+                      <span className="badge border border-1 bg-white text-dark border-secondary">
+                        M
+                      </span>
+                      <span className="badge border border-1 bg-white text-dark border-secondary">
+                        L
+                      </span>
+                      <span className="badge border border-1 bg-white text-dark border-secondary">
+                        XL
+                      </span>
                     </div>
                   </div>
                   <div className="d-flex gap-15 flex-row mt-2 mb-3 align-items-center">
                     <h3 className="product-heading">Quantity :</h3>
                     <div>
-                      <input className="form-control" type="number" name="" min={1} max={10} style={{ "width": "70px" }} />
+                      <input
+                        className="form-control"
+                        type="number"
+                        name=""
+                        min={1}
+                        max={10}
+                        style={{ width: "70px" }}
+                      />
                     </div>
                     <div className="d-flex align-items-center gap-30 ms-5">
                       <button className="button border-0" type="submit">
                         Add to Cart
                       </button>
 
-                      <button className="button signup">
-                        Buy It Now
-                      </button>
+                      <button className="button signup">Buy It Now</button>
                     </div>
                   </div>
                   <div className="d-flex align-items-center gap-30">
                     <div>
-                      <a href="">   <AiOutlineHeart className="fs-5 me-2" />Add to Wishlist</a>
+                      <a href="../Wishlist">
+                        {" "}
+                        <AiOutlineHeart className="fs-5 me-2" />
+                        Add to Wishlist
+                      </a>
                     </div>
                   </div>
                 </div>
@@ -129,7 +171,7 @@ const SingleProduct = () => {
         <div className="container-xxl">
           <div className="row">
             <div className="col-12">
-              <h3 id='review'>Reviews</h3>
+              <h3 id="review">Reviews</h3>
               <div className="review-inner-wrapper mb-5">
                 <div className="review-head d-flex justify-content-between align-items-end">
                   <div>
@@ -155,14 +197,14 @@ const SingleProduct = () => {
                       </a>
                     </div>
                   )}
-
                 </div>
 
-
-
                 <div className="review-form">
-                  <form action="" className="d-flex flex-column gap-15 mb-4" id="review-form">
-
+                  <form
+                    action=""
+                    className="d-flex flex-column gap-15 mb-4"
+                    id="review-form"
+                  >
                     <div>
                       <textarea
                         name=""
