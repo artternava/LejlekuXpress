ce7c29de04b4742dcffaef9614573c93a315ad79
Art Ternava <111341549+artternava@users.noreply.github.com>
2023-05-09 00:11:43 +0200

    Created AuthController

--- LejlekuXpress/Program.cs
+++ LejlekuXpress/Program.cs
@@ -1,6 +1,16 @@
+using LejlekuXpress.Data;
+using LejlekuXpress.Data.ServiceInterfaces;
+using LejlekuXpress.Services;
+using Microsoft.EntityFrameworkCore;
+
 var builder = WebApplication.CreateBuilder(args);
 
+builder.Services.AddDbContext<AppDbContext>(options =>
+                                            options.UseSqlServer(builder.Configuration.GetConnectionString("AppDbContext")));
+
 // Add services to the container.
+
+builder.Services.AddScoped<IAuthService, AuthService>();
 
 builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
