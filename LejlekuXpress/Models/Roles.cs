7a2c30556d6961bea364b8a3115e216bc4b9f3fe
Art Ternava <111341549+artternava@users.noreply.github.com>
2023-05-08 20:25:05 +0200

    Added Roles Model

---
+++ LejlekuXpress/Models/Roles.cs
@@ -0,0 +1,15 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace LejlekuXpress.Models
+{
+    public class Roles
+    {
+        [Key]
+        [DatabaseGenerated(DatabaseGeneratedOption.Identity)]   
+        public int Id { get; set; }
+
+        [Required]
+        public string Type { get; set; }
+    }
+}
