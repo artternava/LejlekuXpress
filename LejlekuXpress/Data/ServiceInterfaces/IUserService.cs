c2031e2564aafe28f3193e58e9bf36622b3cd77a
Andi Ferataj <andi.fr33@gmail.com>
2023-05-10 23:44:21 +0200

    Created User service and interface

---
+++ LejlekuXpress/Data/ServiceInterfaces/IUserService.cs
@@ -0,0 +1,16 @@
+﻿using LejlekuXpress.Data.DTO;
+using LejlekuXpress.Models;
+
+namespace LejlekuXpress.Data.ServiceInterfaces
+{
+    public interface IUserService
+    {
+        Task<User> GetUser(int id);
+
+        Task DeleteUser(int id);
+
+        Task<User> UpdateUser(int id, UserDTO request);
+
+        Task<IEnumerable<User>> GetAll();
+    }
+}
---
+++ LejlekuXpress/Services/UserService.cs
@@ -0,0 +1,89 @@
+﻿using LejlekuXpress.Data;
+using LejlekuXpress.Data.DTO;
+using LejlekuXpress.Data.ServiceInterfaces;
+using LejlekuXpress.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace LejlekuXpress.Services
+{
+    public class UserService : IUserService
+    {
+        private readonly AppDbContext _dbContext;
+
+        public UserService(AppDbContext context)
+        {
+            _dbContext = context;
+        }
+
+        public async Task<User> GetUser(int id)
+        {
+            try
+            {
+                var result = _dbContext.User.Find(id);
+                return result;
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.Message);
+                throw new Exception("An error occurred while attempting to save the user record.");
+            }
+        }
+        public async Task DeleteUser(int id)
+        {
+            try
+            {
+                var result = _dbContext.User.Find(id);
+                if (result != null)
+                {
+                    _dbContext.User.Remove(result);
+                    _dbContext.SaveChanges();
+                }
+
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.Message);
+                throw new Exception("An error occurred while attempting to save the user record.");
+            }
+        }
+        public async Task<User> UpdateUser(int id, UserDTO request)
+        {
+            try
+            {
+                var user = _dbContext.User.Find(id);
+                if (user != null)
+                {
+                    user.FirstName = request.FirstName;
+                    user.LastName = request.LastName;
+                    user.Email = request.Email;
+                    user.PhoneNumber = request.PhoneNumber;
+                    user.ProfilePicture = request.ProfilePicture;
+                    user.RoleId = request.RoleId;
+
+                    _dbContext.SaveChanges();
+                }
+                return user;
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.Message);
+                throw new Exception("An error occurred while attempting to save the user record."); ;
+            }
+        }
+
+        public async Task<IEnumerable<User>> GetAll()
+        {
+            try
+            {
+                var result = await _dbContext.User.ToListAsync();
+                return result;
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.Message);
+                throw new Exception("An error occurred while attempting to save the user record."); ;
+            }
+        }
+
+    }
+}
