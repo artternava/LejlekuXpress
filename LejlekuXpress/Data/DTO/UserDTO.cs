095c03ca42b4da0742f1ea339a745b31687f44ec
Andi Ferataj <andi.fr33@gmail.com>
2023-05-10 23:44:00 +0200

    Create User DTO

---
+++ LejlekuXpress/Data/DTO/UserDTO.cs
@@ -0,0 +1,17 @@
+﻿namespace LejlekuXpress.Data.DTO
+{
+    public class UserDTO
+    {
+        public string FirstName { get; set; }
+
+        public string LastName { get; set; }
+
+        public string Email { get; set; }
+
+        public string PhoneNumber { get; set; }
+
+        public byte[] ProfilePicture { get; set; }
+
+        public int RoleId { get; set; }
+    }
+}
