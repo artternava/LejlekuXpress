c6bc8840a7dfd1a8652249e8e39b8534842f59e2
Andi Ferataj <andi.fr33@gmail.com>
2023-05-10 23:44:32 +0200

    Created User Controller

---
+++ LejlekuXpress/Controllers/UserController.cs
@@ -0,0 +1,81 @@
+﻿using LejlekuXpress.Data.DTO;
+using LejlekuXpress.Data.ServiceInterfaces;
+using Microsoft.AspNetCore.Mvc;
+
+namespace LejlekuXpress.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+
+    public class UserController : ControllerBase
+    {
+        private readonly IUserService _service;
+        public UserController(IUserService service) 
+        {
+            _service = service;
+        }
+
+
+
+        [HttpGet("get")]
+        public async Task<IActionResult> GetUser(int id)
+        {
+            try
+            {
+                var result = await _service.GetUser(id);
+                if (result == null)
+                    return NotFound();
+                return Ok(result);
+            }
+            catch (Exception ex)
+            {
+                return BadRequest(ex.Message);
+            }
+        }
+
+        [HttpDelete("delete")]
+        public async Task<IActionResult> Delete(int id)
+        {
+            try
+            {
+                var result = _service.DeleteUser(id);
+                return Ok(result);
+            }
+            catch (Exception ex)
+            {
+                return BadRequest(ex.Message);
+            }
+        }
+
+        [HttpPut("update")]
+        public async Task<IActionResult> Update(int id, UserDTO request)
+        {
+            try
+            {
+                var result = _service.UpdateUser(id, request);
+                if (result == null)
+                    return NotFound();
+                return Ok(result);
+            }
+            catch (Exception ex)
+            {
+                return BadRequest(ex.Message);
+            }
+        }
+
+        [HttpGet("getall")]
+        public async Task<IActionResult> GetAll()
+        {
+            try
+            {
+                var result = await _service.GetAll();
+                return Ok(result);
+            }
+            catch (Exception ex)
+            {
+                return BadRequest(ex.Message);
+            }
+        }
+
+    }
+}
