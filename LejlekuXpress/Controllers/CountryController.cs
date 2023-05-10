f886bdc0d329d45a9d2119d7a74c8895ec58aa6f
Art Ternava <111341549+artternava@users.noreply.github.com>
2023-05-10 19:54:37 +0200

    Created Country Controller

---
+++ LejlekuXpress/Controllers/CountryController.cs
@@ -0,0 +1,78 @@
+﻿using LejlekuXpress.Data.ServiceInterfaces;
+using LejlekuXpress.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace LejlekuXpress.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class CountryController : ControllerBase
+    {
+        private readonly ICountryService _service;
+        public CountryController(ICountryService service)
+        {
+            _service = service;
+        }
+
+        [HttpPost("add")]
+        public async Task<IActionResult> AddCountry(Country country)
+        {
+            try
+            {
+                _service.AddCountry(country);
+                return Ok(country);
+            }
+            catch (Exception ex)
+            {
+                return BadRequest(ex.Message);
+            }
+        }
+
+        [HttpGet("get")]
+        public async Task<IActionResult> GetCountry (int id)
+        {
+            try
+            {
+                var result = await _service.GetCountry(id);
+                if (result == null)
+                    return NotFound();
+                return Ok(result);
+            }
+            catch (Exception ex)
+            {
+                return BadRequest(ex.Message);
+            }
+        }
+
+        [HttpDelete("delete")]
+        public async Task<IActionResult> Delete(int id)
+        {
+            try
+            {
+                var result = _service.DeleteCountry(id);
+                return Ok(result);
+            }
+            catch (Exception ex)
+            {
+                return BadRequest(ex.Message);
+            }
+        }
+
+        [HttpPut("update")]
+        public async Task<IActionResult> Update(int id, Country country)
+        {
+            try
+            {
+                var result = _service.UpdateCountry(id, country);
+                if (result == null)
+                    return NotFound();
+                return Ok(result);
+            }
+            catch (Exception ex)
+            {
+                return BadRequest(ex.Message);
+            }
+        }
+
+    }
+}
