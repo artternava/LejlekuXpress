a406d605f988d112103fcadf410064f6fa7ee26f
DrenKastratii <121746111+DrenKastratii@users.noreply.github.com>
2023-05-25 10:56:16 +0200

    Created the payment interface, service and DTO(previous DTO commit didn't include PaymentDTO attrbutes).

--- LejlekuXpress/Data/DTO/PaymentDTO.cs
+++ LejlekuXpress/Data/DTO/PaymentDTO.cs
@@ -2,5 +2,11 @@
 {
     public class PaymentDTO
     {
+        public int UserID { get; set; }
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public string CardNumber { get; set; }
+        public string ExpirationDate { get; set; }
+        public string CVV { get; set; }
     }
 }
