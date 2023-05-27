b0ca7d9e0aa73a982adfb64d76352f278a2e9b2a
Art Ternava <111341549+artternava@users.noreply.github.com>
2023-05-27 21:35:05 +0200

    Created BillingInformation model

---
+++ LejlekuXpress/Models/BillingInformation.cs
@@ -0,0 +1,55 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace LejlekuXpress.Models
+{
+    public class BillingInformation
+    {
+        [Key]
+        [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
+        public int Id { get; set; }
+
+        //CARD
+        [Required]
+        public string CardFirstName { get; set; }
+
+        [Required]
+        public string CardLastName { get; set; }
+
+        [Required]
+        public string CardNumber { get; set; }
+
+        [Required]
+        public string ExpirationDate { get; set; }
+
+        [Required]
+        public string CVV { get; set; }
+
+        //ADDRESS
+        [Required]
+        public string FirstName { get; set; }
+
+        [Required]
+        public string LastName { get; set; }
+
+        [Required]
+        public int CountryId { get; set; }
+        public Country Country { get; set; }
+
+        [Required]
+        public string State { get; set; }
+
+        [Required]
+        public string City { get; set; }
+
+        [Required]
+        public string ZipCode { get; set; }
+
+        [Required]
+        public string Address1 { get; set; }
+
+        public string? Address2 { get; set; }
+
+
+    }
+}
