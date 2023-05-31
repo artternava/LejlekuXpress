6a02ddad30f20d2396b9503e56b8075a9aeadacc
Art Ternava <111341549+artternava@users.noreply.github.com>
2023-05-31 21:21:47 +0200

    added ProductDTO

---
+++ LejlekuXpress/Data/DTO/ProductDTO.cs
@@ -0,0 +1,19 @@
+﻿using LejlekuXpress.Models;
+using System.ComponentModel.DataAnnotations;
+
+namespace LejlekuXpress.Data.DTO
+{
+    public class ProductDTO
+    {
+        public int UserId { get; set; }
+        public string Name { get; set; }
+        public int Quantity { get; set; }
+        public string Option1 { get; set; }
+        public string? Option2 { get; set; }
+        public string? Option3 { get; set; }
+        public string? Option4 { get; set; }
+        public string? Option5 { get; set; }
+        public byte[] Images { get; set; }
+        public string Description { get; set; }
+    }
+}
