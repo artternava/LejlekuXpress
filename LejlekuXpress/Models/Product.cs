4304d58bfa2e6b360283aa8fad2fb576a03f6069
Art Ternava <111341549+artternava@users.noreply.github.com>
2023-05-31 21:14:45 +0200

    Created Product Model

---
+++ LejlekuXpress/Models/Product.cs
@@ -0,0 +1,31 @@
+﻿using System.ComponentModel.DataAnnotations.Schema;
+using System.ComponentModel.DataAnnotations;
+
+namespace LejlekuXpress.Models
+{
+    public class Product
+    {
+        [Key]
+        [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
+        public int Id { get; set; }
+
+        [Required]
+        public int UserId { get; set; }
+        public User User { get; set; }
+
+        [Required]
+        public string Name { get; set; }
+
+        [Required]
+        public int Quantity { get; set; }
+
+        [Required]
+        public List<string> Options { get; set; }
+
+        [Required]
+        public byte[] Images { get; set; }
+
+        [Required]
+        public string Description { get; set; }
+    }
+}
