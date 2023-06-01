69c6685d70dc045a0e2c27796328f93cf9427dc1
Art Ternava <111341549+artternava@users.noreply.github.com>
2023-06-01 23:50:27 +0200

    added Product service and interface

---
+++ LejlekuXpress/Data/ServiceInterfaces/IProductService.cs
@@ -0,0 +1,15 @@
+﻿using LejlekuXpress.Data.DTO;
+using LejlekuXpress.Models;
+
+namespace LejlekuXpress.Data.ServiceInterfaces
+{
+    public interface IProductService
+    {
+        Task<Product> AddProduct(ProductDTO request);
+        Task<Product> GetProduct(int id);
+        Task DeleteProduct(int id);
+        Task<Product> UpdateProduct(int id, ProductDTO request);
+        Task<IEnumerable<Product>> GetAll();
+        Task<List<Product>> GetProductByUserId(int ownerId);
+    }
+}
