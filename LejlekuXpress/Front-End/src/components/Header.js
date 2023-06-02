1fd70b1ea562ddd73b74a554f36f2661f2761757
ArditBeqaj <arditi.b75@gmail.com>
2023-06-02 20:12:19 +0200

    Changing the design of navbar menu.

--- LejlekuXpress/Front-End/src/components/Header.js
+++ LejlekuXpress/Front-End/src/components/Header.js
@@ -21,17 +21,17 @@ const Header = () => {
     return (
       <>
         {/* Navbari Kryesor */}
-        <header className="header-upper py-3">
-          <div className="container-xxl px-8">
+        <header className="header-upper">
+          <div className="container-fluid px-8">
             <div className="row align-items-center">
-              <div className="col-3">
-                <h1>
-                  <Link to="" className="logo">
-                    LejlekuX<span className="logo2">Press</span>
-                  </Link>
+              <div className="col-3 px-5">
+                <h1>                 
+                <Link to="">
+                    <img src="images/LejlekuXPress.jpg" className="container-fluid" alt="Lejleku XPress"></img>
+                </Link>
                 </h1>
               </div>
-              <div className="col-4 py-1">
+              <div className="col-5">
                 <div className="input-group">
                   <input
                     type="text"
@@ -45,61 +45,55 @@ const Header = () => {
                   </span>
                 </div>
               </div>
-              <div className="col-5">
-                <div className="header-upper-links d-flex align-items-center justify-contect-between gap-45">
-                  <div>
-                    <Link
-                      to="Wishlist"
-                      className="d-flex align-items-center gap-10 text-white"
-                    >
-                      <img src="../images/wishlist.svg" alt="wishlist" />
-                      <p className="mb-0">
-                        Favorite <br /> Wishlists
-                      </p>
-                    </Link>
-                  </div>
-                  <div>
-                    <Link
-                      to="Cart"
-                      className="d-flex align-items-center gap-10 text-white"
-                    >
-                      <img src="../images/cart.svg" alt="cart" />
-                      <div className="d-flex flex-column gap-10">
-                        <span className="badge bg-white text-dark"></span>
-                        <p className="mb-0">$ 0.00</p>
-                      </div>
-                    </Link>  
-                  </div>
-                  <div>
-                    {isLoggedIn ? (
-                      <>
-                      <div className="d-flex align-items-center gap-10 text-white">
-                        <Link to="userdashboard" className="d-flex align-items-center gap-10 text-white">
-                          <img src="../images/user.svg" alt="user" />
-                          <p className="mb-0">
-                            My <br />Profile 
-                          </p>
-                        </Link>
-                        <button className="btn btn-outline-light text-white text-decoration-none" onClick={handleLogout}>
-                          <i className="bi bi-lock bi-lg"></i>
-                          <span>Logout</span>
-                        </button>
-                      </div>
-                    </>                    
-                    ) : (
-                      <Link
-                        to="login"
-                        className="d-flex align-items-center gap-10 text-white"
-                      >
-                        <img src="../images/user.svg" alt="user" />
-                        <p className="mb-0">
-                          Login <br /> My Account
-                        </p>
-                      </Link>
-                    )}
-                  </div>
-                </div>
-              </div>
+              <div className="col-3">
+  <div className="header-upper-links d-flex align-items-center justify-content-start gap-3">
+  
+    <div>
+      <Link to="Wishlist" className="d-flex align-items-center gap-2 text-black btn btn-outline-light text-black text-decoration-none">
+        <i className="bi bi-heart fs-3"></i>
+        <p className="mb-0">Wishlists</p>
+      </Link>
+    </div>
+    
+    
+    <div>
+      <Link to="Cart" className="d-flex align-items-center gap-10 text-black btn btn-outline-light text-black text-decoration-none">
+        <i className="bi bi-cart4 fs-3"></i>
+        <div className="d-flex flex-column gap-10">
+          <span className="badge bg-white text-dark">$ 0.00</span>
+          
+        </div>
+      </Link>
+    </div>
+    
+    <div className="ml-auto">
+      {isLoggedIn ? (
+        <>
+          <div className="d-flex align-items-center gap-10 text-black">
+         
+            <Link to="userdashboard" className="d-flex align-items-center gap-10 text-black btn btn-outline-light text-black text-decoration-none">
+            <i className="d-flex bi bi-person fs-3"></i>
+              <p className="mb-0">My Profile</p>
+            </Link>
+                   
+              <i className="bi bi-lock bi-lg"></i>
+              <span>Logout</span>
+           
+          </div>
+        </>
+      ) : (
+        
+        <Link to="login" className="d-flex align-items-center gap-10 text-black container-fluid btn btn-outline-light text-black text-decoration-none">
+          <i className="d-flex bi bi-person fs-3"></i>
+          <p className="mb-0">Login</p>
+        </Link>
+        
+      )}
+    </div>
+  </div>
+</div>
+
+
             </div>
           </div>
         </header>
