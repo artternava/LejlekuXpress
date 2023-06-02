094ea865168b0b782116f5e02aca408efaae822f
DrenKastratii <121746111+DrenKastratii@users.noreply.github.com>
2023-06-02 11:12:24 +0200

    Added the controller for the Category Model.

---
+++ LejlekuXpress/Controllers/CategoryController.cs
@@ -0,0 +1,49 @@
+﻿using LejlekuXpress.Data.ServiceInterfaces;
+using LejlekuXpress.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace LejlekuXpress.Controllers
+
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class CategoryController : ControllerBase
+    {
+        private readonly ICategoryService _service;
+        public CategoryController(ICategoryService service)
+        {
+            _service = service;
+        }
+
+        [HttpGet("get")]
+        public async Task<IActionResult> GetCategory(int id)
+        {
+            try
+            {
+                var result = await _service.GetCategory(id);
+                if (result == null)
+                    return NotFound();
+                return Ok(result);
+            }
+            catch (Exception ex)
+            {
+                return BadRequest(ex.Message);
+            }
+        }
+
+        [HttpGet("getall")]
+        public async Task<IActionResult> GetAll()
+        {
+            try
+            {
+                var result = await _service.GetAll();
+                return Ok(result);
+            }
+            catch (Exception ex)
+            {
+                return BadRequest(ex.Message);
+            }
+        }
+
+    }
+}
