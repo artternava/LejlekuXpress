f12cac37903b88fb487d5cdb3392ad8e6aaf179c
ArditBeqaj <arditi.b75@gmail.com>
2023-06-02 21:18:57 +0200

    Added a "Link" to footer.

--- LejlekuXpress/Front-End/src/components/Footer.js
+++ LejlekuXpress/Front-End/src/components/Footer.js
@@ -1,4 +1,5 @@
-import React from 'react'
+import React from 'react';
+import { Link } from 'react-router-dom';
 
 const Footer = () => {
   return (
@@ -10,6 +11,9 @@ const Footer = () => {
               <p className='text-center mb-0 text-white'>
                 &copy; {new Date().getFullYear()}: LejlekuXPress. All rights reserved.
               </p>
+              <div className="text-center">
+                <Link to="contact">Contact</Link>
+              </div>
             </div>
           </div>
         </div>
