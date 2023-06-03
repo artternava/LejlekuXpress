4167c569ea43402a9aa5984ea651bda5e550922b
Art Ternava <111341549+artternava@users.noreply.github.com>
2023-06-03 02:20:17 +0200

    Migrated CategoryId column add to Product model

---
+++ LejlekuXpress/Migrations/20230602224703_addedCategoryIdReferenceToProduct.cs
@@ -0,0 +1,68 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace LejlekuXpress.Migrations
+{
+    /// <inheritdoc />
+    public partial class addedCategoryIdReferenceToProduct : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<bool>(
+                name: "IsApproved",
+                table: "Product",
+                type: "bit",
+                nullable: true,
+                oldClrType: typeof(bool),
+                oldType: "bit");
+
+            migrationBuilder.AddColumn<int>(
+                name: "CategoryId",
+                table: "Product",
+                type: "int",
+                nullable: false,
+                defaultValue: 0);
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Product_CategoryId",
+                table: "Product",
+                column: "CategoryId");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_Product_Category_CategoryId",
+                table: "Product",
+                column: "CategoryId",
+                principalTable: "Category",
+                principalColumn: "Id",
+                onDelete: ReferentialAction.Cascade);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_Product_Category_CategoryId",
+                table: "Product");
+
+            migrationBuilder.DropIndex(
+                name: "IX_Product_CategoryId",
+                table: "Product");
+
+            migrationBuilder.DropColumn(
+                name: "CategoryId",
+                table: "Product");
+
+            migrationBuilder.AlterColumn<bool>(
+                name: "IsApproved",
+                table: "Product",
+                type: "bit",
+                nullable: false,
+                defaultValue: false,
+                oldClrType: typeof(bool),
+                oldType: "bit",
+                oldNullable: true);
+        }
+    }
+}
