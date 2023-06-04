73fd6dcf58868e35bbf0357f37260f147e2b33b5
Art Ternava <111341549+artternava@users.noreply.github.com>
2023-06-04 02:59:52 +0200

    Created Roles Controller

---
+++ LejlekuXpress/Controllers/RolesController.cs
@@ -0,0 +1,50 @@
+﻿using LejlekuXpress.Data.ServiceInterfaces;
+using Microsoft.AspNetCore.Mvc;
+
+namespace LejlekuXpress.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class RolesController : ControllerBase
+    {
+        private readonly IRolesService _service;
+        public RolesController(IRolesService service)
+        {
+            _service = service;
+        }
+
+        #region GetById
+        [HttpGet("get")]
+        public async Task<IActionResult> GetRole(int id)
+        {
+            try
+            {
+                var result = await _service.GetRole(id);
+                if (result == null)
+                    return NotFound();
+                return Ok(result);
+            }
+            catch (Exception ex)
+            {
+                return BadRequest(ex.Message);
+            }
+        }
+        #endregion
+
+        #region GetAll
+        [HttpGet("getall")]
+        public async Task<IActionResult> GetAll()
+        {
+            try
+            {
+                var result = await _service.GetAll();
+                return Ok(result);
+            }
+            catch (Exception ex)
+            {
+                return BadRequest(ex.Message);
+            }
+        }
+        #endregion
+    }
+}
