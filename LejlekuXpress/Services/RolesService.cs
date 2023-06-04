c8ef9408ebf19ae8ae9b389a3f4f11ca35221b85
Art Ternava <111341549+artternava@users.noreply.github.com>
2023-06-04 02:55:06 +0200

    Created Roles Service

---
+++ LejlekuXpress/Services/RolesService.cs
@@ -0,0 +1,47 @@
+﻿using LejlekuXpress.Data;
+using LejlekuXpress.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace LejlekuXpress.Services
+{
+    public class RolesService
+    {
+        private readonly AppDbContext _context;
+        public RolesService(AppDbContext context)
+        {
+            _context = context;
+        }
+
+        #region GetAll
+        public async Task<IEnumerable<Roles>> GetAll()
+        {
+            try
+            {
+                var result = await _context.Roles.ToListAsync();
+                return result;
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.Message);
+                throw new Exception("An error occurred while attempting to save the user record."); ;
+            }
+        }
+        #endregion
+
+        #region GetById
+        public async Task<Roles> GetRole(int id)
+        {
+            try
+            {
+                var result = _context.Roles.Find(id);
+                return result;
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.Message);
+                throw new Exception("An error occurred while attempting to get the category name.");
+            }
+        }
+        #endregion
+    }
+}
