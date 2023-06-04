37555fecad66c958edce5cd2e5a8e0fe937e56c5
Art Ternava <111341549+artternava@users.noreply.github.com>
2023-06-04 02:18:57 +0200

    added the priviledge condition to the sidebar

--- LejlekuXpress/Front-End/src/components/SideBar.js
+++ LejlekuXpress/Front-End/src/components/SideBar.js
@@ -1,6 +1,17 @@
 import React from "react";
+import useAuthToken from './useAuthToken.js';
 
 function Sidebar({ handleTabClick }) {
+  const { userRole } = useAuthToken();
+  function showListings() {
+    console.log('userRole:', userRole);
+    if (userRole === 4) {
+      return false;
+    }
+    return true;
+  }
+
+  console.log('showListings:', showListings());
 
   return (
     <div className="sidebar">
@@ -8,37 +19,39 @@ function Sidebar({ handleTabClick }) {
         <div className="sidebarMenu">
           <h3 className="sidebarTitle">My Information</h3>
           <ul className="sidebarList">
-          <li className="sidebar-item" onClick={() => handleTabClick("personal")}>
-            <i class="bi bi-person-fill"></i>
-            <span className="ms-2">My Profile</span>
-           </li>
-           <li className="sidebar-item" onClick={() => handleTabClick("orders")}>
-            <i class="bi bi-bag-check"></i> 
-            <span className="ms-2">My Orders</span>
-          </li>
-          <li className="sidebar-item" onClick={() => handleTabClick("shipping")}>
-            <i class="bi bi-truck"></i>
-            <span className="ms-2">Shipping Address</span>
-          </li>
-          <li className="sidebar-item" onClick={() => handleTabClick("payment")}>
-            <i class="bi bi-credit-card-fill"></i>
-            <span className="ms-2">Payment Methods</span>
-          </li>
-          <li className="sidebar-item" onClick={() => handleTabClick("changepassword")}>
-            <i class="bi bi-lock-fill"></i>
-            <span className="ms-2">Change Password</span>
-          </li>
-          </ul>
-        </div>
-        <div className="sidebarMenu">
-          <h3 className="sidebarTitle">My Seller Dashboard</h3>
-          <ul className="sidebarList">
-            <li className="sidebar-item" onClick={() => handleTabClick("listings")}>
-            <i class="bi bi-view-list"></i>
-            <span className="ms-2">My Listings</span>
+            <li className="sidebar-item" onClick={() => handleTabClick("personal")}>
+              <i className="bi bi-person-fill"></i>
+              <span className="ms-2">My Profile</span>
+            </li>
+            <li className="sidebar-item" onClick={() => handleTabClick("orders")}>
+              <i className="bi bi-bag-check"></i>
+              <span className="ms-2">My Orders</span>
+            </li>
+            <li className="sidebar-item" onClick={() => handleTabClick("shipping")}>
+              <i className="bi bi-truck"></i>
+              <span className="ms-2">Shipping Address</span>
+            </li>
+            <li className="sidebar-item" onClick={() => handleTabClick("payment")}>
+              <i className="bi bi-credit-card-fill"></i>
+              <span className="ms-2">Payment Methods</span>
+            </li>
+            <li className="sidebar-item" onClick={() => handleTabClick("changepassword")}>
+              <i className="bi bi-lock-fill"></i>
+              <span className="ms-2">Change Password</span>
             </li>
           </ul>
         </div>
+        {userRole !== '4' && (
+          <div className="sidebarMenu">
+            <h3 className="sidebarTitle">My Seller Dashboard</h3>
+            <ul className="sidebarList">
+              <li className="sidebar-item" onClick={() => handleTabClick("listings")}>
+                <i className="bi bi-view-list"></i>
+                <span className="ms-2">My Listings</span>
+              </li>
+            </ul>
+          </div>
+        )}
       </div>
     </div>
   );
