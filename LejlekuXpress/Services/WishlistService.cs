0bf4f3f49549d97e920ed6be8e2a778df40c8891
ArditBeqaj <arditi.b75@gmail.com>
2023-06-05 19:00:58 +0200

    added dependency injection on WishlistService

--- LejlekuXpress/Services/WishlistService.cs
+++ LejlekuXpress/Services/WishlistService.cs
@@ -1,6 +1,15 @@
-﻿namespace LejlekuXpress.Services
+﻿using LejlekuXpress.Data;
+
+namespace LejlekuXpress.Services
 {
     public class WishlistService
     {
+        private readonly AppDbContext _context;
+
+        public WishlistService(AppDbContext context)
+        {
+            _context = context;
+        }
+
     }
 }
