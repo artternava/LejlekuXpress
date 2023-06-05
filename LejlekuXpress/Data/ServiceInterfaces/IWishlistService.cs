6bc87865244860a685b07eca07d66c12bfd45e27
ArditBeqaj <arditi.b75@gmail.com>
2023-06-05 19:13:14 +0200

    Added GetByUserId method in IWishlistService

--- LejlekuXpress/Data/ServiceInterfaces/IWishlistService.cs
+++ LejlekuXpress/Data/ServiceInterfaces/IWishlistService.cs
@@ -6,5 +6,7 @@ namespace LejlekuXpress.Data.ServiceInterfaces
     public interface IWishlistService
     {
         Task<Wishlist> AddItem(WishlistDTO request);
+
+        Task<List<Wishlist>> GetByUserId(int userId);
     }
 }
--- LejlekuXpress/Front-End/src/pages/Home.js
+++ LejlekuXpress/Front-End/src/pages/Home.js
@@ -15,7 +15,9 @@ const Home = () => {
         <link rel="canonical" href="http://mysite.com/example" />
       </Helmet>
 
-      <div className="row mt-5">
+      <section className="special-wrapper py-5 home-wrapper-2">
+        <div className="container-xxl">
+        <div className="row mb-5">
             <div className="carousel-wrapper">
               <Carousel
                 showArrows={true}
@@ -40,14 +42,14 @@ const Home = () => {
               </Carousel>
             </div>
           </div>
-      
-      
-     
-      <section className="special-wrapper py-5 home-wrapper-2">
+          </div>
+          </section>
+  
+          <section className="special-wrapper py-5 home-wrapper-2 ">
         <div className="container-xxl">
-          <div className="row">
-            <div className="col-12">
-              <h3 className="section-heading">Special Products</h3>
+          <div className="row ">
+            <div className="col-12 ">
+              <h3 className="section-heading ">Special Products</h3>
             </div>
           </div>
           <div className="row">
@@ -56,6 +58,8 @@ const Home = () => {
           </div>
         </div>
       </section>
+
+
     </>
   );
 };
