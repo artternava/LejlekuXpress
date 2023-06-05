4b8626eeb60e08b9a85d1138865b48067fb64b5a
ArditBeqaj <arditi.b75@gmail.com>
2023-06-05 19:15:53 +0200

    Created wishlist Controller

---
+++ LejlekuXpress/Controllers/WishlistController.cs
@@ -0,0 +1,17 @@
+﻿using LejlekuXpress.Data.ServiceInterfaces;
+using Microsoft.AspNetCore.Mvc;
+
+namespace LejlekuXpress.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class WishlistController : ControllerBase
+    {
+        private readonly IWishlistService _service;
+
+        public WishlistController(IWishlistService service)
+        {
+            _service = service;
+        }
+    }
+}
