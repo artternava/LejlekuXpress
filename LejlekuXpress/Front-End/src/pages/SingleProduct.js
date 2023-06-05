b8f94800893b2dcc0333217bbaa3f3a758db90d7
Art Ternava <111341549+artternava@users.noreply.github.com>
2023-06-05 23:59:49 +0200

    added handleAdd in single product

--- LejlekuXpress/Front-End/src/pages/SingleProduct.js
+++ LejlekuXpress/Front-End/src/pages/SingleProduct.js
@@ -29,6 +29,24 @@ const SingleProduct = () => {
     }
   };
 
+  async function handleAdd(productId) {
+    try {
+      const itemsData ={
+        UserId: parseInt(userId, 10),
+        ProductId: productId,
+      }
+      console.log(itemsData);
+      setAddItem(itemsData)
+      await axios.post('http://localhost:39450/api/Wishlist/add', addItem)
+      .then((response) => {
+        window.alert('add successful');
+      });
+      fetchWishlist(); 
+    } catch (error) {
+      console.error('Error adding items:', error);
+    }
+  }
+
   async function fetchWishlist() {
     try {
       const response = await axios.get(`http://localhost:39450/api/Wishlist/getbyuserid?userID=${userId}`);
