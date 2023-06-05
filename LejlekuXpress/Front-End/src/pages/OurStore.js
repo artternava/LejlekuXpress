480c782b5e65a510c1449751b4f295159b5b87e4
Art Ternava <111341549+artternava@users.noreply.github.com>
2023-06-05 23:12:09 +0200

    modify return on OurStore

--- LejlekuXpress/Front-End/src/pages/OurStore.js
+++ LejlekuXpress/Front-End/src/pages/OurStore.js
@@ -1,205 +1,92 @@
 import React, { useState, useEffect } from "react";
+import axios from 'axios';
 import { useLocation } from "react-router-dom";
 import Meta from '../components/Meta'
 import ReactStars from "react-rating-stars-component";
 import ProductCard from "../components/ProductCard"
 import Header from "../components/Header"
+import useAuthToken from '../components/useAuthToken';
+
 
 const OurStore = () => {
   const location = useLocation();
   const searchParams = new URLSearchParams(location.search);
   const searchQuery = searchParams.get("search");
+  const [items, setItems] = useState(null);
+  const [wishlist, setWishlist] = useState(null);
+  const [addItem, setAddItem] = useState(null);
+  const { userId } = useAuthToken();
 
   useEffect(() => {
+    
     console.log('Search Query in ourStore:', searchQuery);
   }, [searchQuery]);
 
+  
+
+  
+
+  
+
+  
 
   return (
     <>
     <Meta title="Our Store"></Meta>
     <div className='store-wrapper home-wrapper-2 py5'>
-      <div className='container-xxl'>
-        <div className='row'>
-          <div className='col-3'>
-              <div className='filter-card mb-3'>
-                <h3 className='filter-title'>Shop By Categories</h3>
-                <div>
-                  <ul className='ps-0'>
-                    <li>Watch</li>
-                    <li>Tv</li>
-                    <li>Camera</li>
-                    <li>Laptop</li>
-                  </ul>
-                </div>
-              </div>
-              <div className='filter-card mb-3'>
-                <h3 className='filter-title'>Filter By</h3>
-                <div>
-                  <h5 className='sub-title'>Availability</h5>
-              <div>
-              <div className='form-check'>
-                    <input className='form-check-input' type='checkbox' value='' id=''></input>
-                      <label className='form-check-label' htmlfor=''>
-                        In stock
-                        </label>
-                    
-                  </div>
-                  <div className='form-check'>
-                    <input className='form-check-input' type='checkbox' value='' id=''></input>
-                      <label className='form-check-label' htmlfor=''>
-                        Out of stock
-                        </label>
-                    
-                  </div>
-              </div>
-              <h5 className='sub-title'>Price</h5>
-              <div className='d-flex align-items-center gap-10'>
-              <div className="form-floating">
-                  <input type="email" className="form-control py-1" id="floatingInput" placeholder="From"/>
-                  <label htmlfor="floatingInput">
-                    From
-                  </label>
-              </div>
-              <div className="form-floating">
-                  <input type="email" className="form-control py-1" id="floatingInput" placeholder="To"/>
-                  <label htmlfor="floatingInput">
-                    To
-                  </label>
-              </div>
-              </div>
-              <h5 className='sub-title'>Color</h5>
-              <div className='d-flex flex-wrap'>
-                <ul className='colors ps-0' >
-                  <li></li>
-                  <li></li>
-                  <li></li>
-                  <li></li>
-                  <li></li>
-                  <li></li>
-                  <li></li>
-                  <li></li>
-                  <li></li>
-                  <li></li>
-                  <li></li>
-                  <li></li>
-                  <li></li>
-                  <li></li>
-                  <li></li>
-                  <li></li>
-                  <li></li>
-                  <li></li>
-                </ul>
-              </div>
-                </div>
-                <h5 className='sub-title'>Size</h5>
-                <div>
-                <div className='form-check'>
-                    <input className='form-check-input' type='checkbox' value='' id='color-1'></input>
-                      <label className='form-check-label' htmlfor='color'>
-                        S (2)
-                        </label>
-                    
-                  </div>
-                  <div className='form-check'>
-                    <input className='form-check-input' type='checkbox' value='' id='color-2'></input>
-                      <label className='form-check-label' htmlfor='color-2'>
-                        M (2)
-                        </label>
-                    
-                  </div>
-                  <div className='form-check'>
-                    <input className='form-check-input' type='checkbox' value='' id='color-3'></input>
-                      <label className='form-check-label' htmlfor='color-3'>
-                        XXL (2)
-                        </label>
-                    
-                  </div>
-                </div>
-              </div>
-              <div className='filter-card mb-3'>
-                <h3 className='filter-title'>Product Tags</h3>
-                <div>
-                  <div className='product-tag d-flex flex-wrap align-items-center gap-10'>
-                        <span className='badge bg-light text-secondary rounded-3 py-2 px-3'>
-                          Headphones
-                        </span>
-                        <span className='badge bg-light text-secondary rounded-3 py-2 px-3'>
-                          Laptop
-                        </span>
-                        <span className='badge bg-light text-secondary rounded-3 py-2 px-3'>
-                          Mobile
-                        </span>
-                        <span className='badge bg-light text-secondary rounded-3 py-2 px-3'>
-                          Wire
-                        </span>
-                        
-                  </div>
-                </div>
-              </div>
-              <div className='filter-card mb-3'>
-                <h3 className='filter-title'>Random Product</h3>
-                <div>
-                  <div className='random-products mb-3 d-flex'>
-                    <div className='w-50'>
-                        <img src='images/watch.jpg' className='img-fluid' alt='watch'></img>
-                    </div>
-                    <div className='w-50'>
-                    <h5> Kids headphones bulk 10 pack multi colored</h5>
-                    <ReactStars count={5} size={24} value="3" edit={false} activeColor="#ffd700"/>
-                    <b>$ 300</b>
-                    </div>
-                  </div>
-                  <div className='random-products d-flex'>
-                    <div className='w-50'>
-                        <img src='images/watch.jpg' className='img-fluid' alt='watch'></img>
-                    </div>
-                    <div className='w-50'>
-                    <h5> Kids headphones bulk 10 pack multi colored</h5>
-                    <ReactStars count={5} size={24} value="3" edit={false} activeColor="#ffd700"/>
-                    <b>$ 300</b>
+    {items && items.map((item) => (
+        <section style={{ backgroundColor: "#fff" }}>
+          <div className="container py-3">
+            <div className="row justify-content-center">
+              <div className="col-md-12 col-xl-10">
+                <div className="card shadow-0 border rounded-3">
+                  <div className="card-body">
+                    <div className="row">
+                      <div className="col-md-12 col-lg-3 col-xl-3 mb-4 mb-lg-0">
+                        <div className="bg-image hover-zoom ripple rounded ripple-surface">
+                          <img src={`data:image/${item.image};base64,${item.image}`} alt="Product" style={{ width: "60%", aspectRatio: "1/1" }} />
+                          <a href="#!">
+                            <div className="hover-overlay">
+                              <div className="mask" style={{ backgroundColor: "rgba(253, 253, 253, 0.15)" }}></div>
+                            </div>
+                          </a>
+                        </div>
+                      </div>
+                      <div className="col-md-6 col-lg-6 col-xl-6">
+                        <h5>{item.name}</h5>
+                        <div className="d-flex flex-row">
+                          <div className="text-danger mb-1 me-2">
+                            <p><b>Quantity</b></p>
+                          </div>
+                          <span>{item.quantity}</span>
+                        </div>
+                        <div className="mt-1 mb-0 text-muted small">
+                          <p>{item.specifications}</p>
+                        </div>
+                        <p className="text-truncate mb-4 mb-md-0">
+                          {item.description}
+                        </p>
+                      </div>
+                      <div className="col-md-6 col-lg-3 col-xl-3 border-sm-start-none border-start">
+                        <div className="d-flex flex-row align-items-center mb-1">
+                          <h4 className="mb-1 me-1">${item.price}</h4>
+                        </div>
+                        <h6 className="text-success">${item.shippingPrice}</h6>
+                        <div className="d-flex flex-column mt-4">
+                          <button className="btn btn-primary btn-sm" type="button">Add to Cart</button>
+                          <button className="btn btn-danger btn-sm mt-2" type="button" onClick={() => handleAdd(item.id)}>
+                            <i className="bi bi-heart-fill me-2"></i>Add to Wishlist
+                          </button>
+                        </div>
+                      </div>
                     </div>
                   </div>
                 </div>
-              </div>
-          </div>
-          <div className='col-9'>
-            <div className='filter-sort-grid mb-4'>
-            <div className='d-flex justify-content-between align-items-center'>
-            <div className='d-flex align-items-center gap-10'>
-                <p className='mb-0 d-block' style={{width:"100px"}}>Sort By:</p>
-                <select name='' className='form-control form-select' id=''>
-                  <option value="manual">Featured</option>
-                  <option value="best-selling" selected="selected">Best selling</option>
-                  <option value="title-ascending">Alphabetically, A-Z</option>
-                  <option value="title-descending">Alphabetically, Z-A</option>
-                  <option value="price-ascending">Price, low to high</option>
-                  <option value="price-descending">Price, high to low</option>
-                  <option value="created-ascending">Date, old to new</option>
-                  <option value="created-descending">Date, new to old</option>
-                </select>
-              </div>
-              <div className='d-flex align-items-center gap-10'>
-                <p className='totalproducts'>21 Products</p>
-                <div className='d-flex gap-10 align-items-center'></div>
-
               </div>
             </div>
-            </div>
-            <div className='products-list pb-5'>
-              <div className='d-flex gap-2 flex-wrap ' style={{width: '130%', margin: '0 auto'}}>
-              <ProductCard/>
-              <ProductCard/>
-              <ProductCard/>
-              <ProductCard/>
-              <ProductCard/>
-              <ProductCard/>
-              <ProductCard/>
-              </div>
-            </div>           
           </div>
-        </div>
-      </div>
+        </section>
+      ))}
     </div>
     </>
   )
