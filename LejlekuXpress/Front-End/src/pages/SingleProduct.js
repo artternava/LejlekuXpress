b9c2ca50ade12a420efc82b0d4ad3b9176d8ad9c
Art Ternava <111341549+artternava@users.noreply.github.com>
2023-06-05 23:58:13 +0200

    changed return in single product

--- LejlekuXpress/Front-End/src/pages/SingleProduct.js
+++ LejlekuXpress/Front-End/src/pages/SingleProduct.js
@@ -1,17 +1,14 @@
-import React from "react";
-import { useState } from "react";
+import React, { useState, useEffect } from "react";
+import axios from 'axios';
 import ReactStars from "react-stars";
 import ReactImageZoom from "react-image-zoom";
 import { AiOutlineHeart } from "react-icons/ai";
+import { useParams } from "react-router-dom";
+import useAuthToken from '../components/useAuthToken';
 
 const SingleProduct = () => {
-  const props = {
-    width: 400,
-    height: 500,
-    zoomWidth: 600,
-    img: "https://images.pexels.com/photos/190819/pexels-photo-190819.jpeg?cs=srgb&dl=pexels-fernando-arcos-190819.jpg&fm=jpg",
-  };
-  const [orderedProduct] = useState(true);
+  
+
   return (
     <>
       <div className="main-product-wrapper py-5 home-wrapper-2">
@@ -20,108 +17,36 @@ const SingleProduct = () => {
             <div className="col-6">
               <div className="main-product-image">
                 <div>
-                  <ReactImageZoom {...props} />
-                </div>
-              </div>
-              <div className="other-product-images d-flex flex-wrap gap-15">
-                <div>
-                  <img
-                    src="https://images.pexels.com/photos/190819/pexels-photo-190819.jpeg?cs=srgb&dl=pexels-fernando-arcos-190819.jpg&fm=jpg"
-                    className="img-fluid"
-                    alt=""
-                  />
-                </div>
-                <div>
-                  <img
-                    src="https://images.pexels.com/photos/190819/pexels-photo-190819.jpeg?cs=srgb&dl=pexels-fernando-arcos-190819.jpg&fm=jpg"
-                    className="img-fluid"
-                    alt=""
-                  />
-                </div>
-                <div>
-                  <img
-                    src="https://images.pexels.com/photos/190819/pexels-photo-190819.jpeg?cs=srgb&dl=pexels-fernando-arcos-190819.jpg&fm=jpg"
-                    className="img-fluid"
-                    alt=""
-                  />
-                </div>
-                <div>
-                  <img
-                    src="https://images.pexels.com/photos/190819/pexels-photo-190819.jpeg?cs=srgb&dl=pexels-fernando-arcos-190819.jpg&fm=jpg"
-                    className="img-fluid"
-                    alt=""
-                  />
+                  <img src={`data:image/${getImageExtension(items.image)};base64,${items.image}`} alt="Product" style={{width: "60%", aspectRatio: "1/1"}} />
                 </div>
               </div>
             </div>
             <div className="col-6">
               <div className="main-product-details">
                 <div className="border-bottom">
-                  <h3 className="title">Luxury Handcrafted Wrist Watch</h3>
+                  <h1 className="title">{items.name}</h1>
                 </div>
                 <div className="border-bottom py-3">
-                  <p className="price">$ 100</p>
-                  <div className="d-flex align-items-center gap-10">
-                    <ReactStars
-                      count={5}
-                      size={24}
-                      value={4}
-                      edit={false}
-                      activeColor="#ffd700"
-                    />
-                    <p className="mb-0 t-review">(2 reviews)</p>
-                  </div>
-                  <a className="review-btn" href="#review">
-                    Write a review
-                  </a>
+                  <h4 className="price">${items.price}</h4>
+                  <p className="price text-success">${items.shippingPrice}</p>
                 </div>
                 <div className="border-bottom py-3">
-                  <div className="d-flex gap-10 align-items-center my-2">
-                    <h3 className="product-heading">Type :</h3>
-                    <p className="product-data">Watch</p>
-                  </div>
-                  <div className="d-flex gap-10 align-items-center my-2">
-                    <h3 className="product-heading">Brand :</h3>
-                    <p className="product-data">Casio</p>
-                  </div>
-                  <div className="d-flex gap-10 align-items-center my-2">
-                    <h3 className="product-heading">Category :</h3>
-                    <p className="product-data">Accesoris</p>
-                  </div>
-                  <div className="d-flex gap-10 align-items-center my-2">
-                    <h3 className="product-heading">Tags :</h3>
-                    <p className="product-data">Wrist Watch</p>
-                  </div>
-                  <div className="d-flex gap-10 align-items-center my-2">
-                    <h3 className="product-heading">Availability :</h3>
-                    <p className="product-data">In Stock</p>
-                  </div>
-                  <div className="d-flex gap-10 flex-column mt-2 mb-3">
-                    <h3 className="product-heading">Size :</h3>
-                    <div className="d-flex flex-wrap gap-15">
-                      <span className="badge border border-1 bg-white text-dark border-secondary">
-                        S
-                      </span>
-                      <span className="badge border border-1 bg-white text-dark border-secondary">
-                        M
-                      </span>
-                      <span className="badge border border-1 bg-white text-dark border-secondary">
-                        L
-                      </span>
-                      <span className="badge border border-1 bg-white text-dark border-secondary">
-                        XL
-                      </span>
-                    </div>
-                  </div>
+                <div className="mt-1 mb-0 text-muted small">
+                  <p style={{fontSize: "16px"}}>{items.specifications}</p>
+                </div>
+                <div className="mt-1 mb-0 ">
+                  <p>{items.description}</p>
+                </div>
                   <div className="d-flex gap-15 flex-row mt-2 mb-3 align-items-center">
                     <h3 className="product-heading">Quantity :</h3>
                     <div>
+                      
                       <input
                         className="form-control"
                         type="number"
                         name=""
                         min={1}
-                        max={10}
+                        max={items.quantity}
                         style={{ width: "70px" }}
                       />
                     </div>
@@ -133,16 +58,16 @@ const SingleProduct = () => {
                       <button className="btn btn-success me-2">Buy It Now</button>
                     </div>
                   </div>
+                  <div className="mt-1 mb-0 ">
+                      <p>Available: {items.quantity}</p>
+                    </div>
                   <div className="d-flex align-items-center gap-30">
                     <div>
-                      <a href="../Wishlist">
-                        {" "}
-                        <AiOutlineHeart className="fs-5 me-2" />
-                        Add to Wishlist
-                      </a>
+                      <button className="btn btn-outline-secondary" type="button" onClick={() => handleAdd(items.id)}><i className="bi bi-heart-fill me-2"></i>Add to Wishlist</button>
                     </div>
                   </div>
                 </div>
+                
               </div>
             </div>
           </div>
@@ -155,93 +80,7 @@ const SingleProduct = () => {
             <div className="col-12">
               <h4>Description</h4>
               <div className="bg-white p-3">
-                <p>
-                  Lorem ipsum dolor sit, amet consectetur adipisicing elit.
-                  Ipsam dolore a ab dolorum reiciendis maiores, aliquam aliquid
-                  adipisci accusantium ut at unde vitae distinctio, minus
-                  voluptatum suscipit animi esse totam.
-                </p>
-              </div>
-            </div>
-          </div>
-        </div>
-      </section>
-
-      <section className="reviews-wrapper home-wrapper-2 ">
-        <div className="container-xxl">
-          <div className="row">
-            <div className="col-12">
-              <h3 id="review">Reviews</h3>
-              <div className="review-inner-wrapper mb-5">
-                <div className="review-head d-flex justify-content-between align-items-end">
-                  <div>
-                    <h4 className="mb-2">Customer Reviews</h4>
-                    <div className="d-flex align-items-center gap-10">
-                      <ReactStars
-                        count={5}
-                        size={24}
-                        value={4}
-                        edit={false}
-                        activeColor="#ffd700"
-                      />
-                      <p className="mb-0">Based on 2 reviews</p>
-                    </div>
-                  </div>
-                  {orderedProduct && (
-                    <div>
-                      <a
-                        className="text-dark text-decoration-underline"
-                        href="#review-form"
-                      >
-                        Write a review
-                      </a>
-                    </div>
-                  )}
-                </div>
-
-                <div className="review-form">
-                  <form
-                    action=""
-                    className="d-flex flex-column gap-15 mb-4"
-                    id="review-form"
-                  >
-                    <div>
-                      <textarea
-                        name=""
-                        id=""
-                        className="w-100 form-control mb-4"
-                        cols="30"
-                        rows="4"
-                        placeholder="Comments"
-                      ></textarea>
-                    </div>
-                    <div>
-                      <button className="btn btn-primary me-2">Submit</button>
-                    </div>
-                  </form>
-                </div>
-                <div className="reviews mt-4 ">
-                  <div className="review">
-                    <div className="d-flex gap-10 align-items-center ">
-                      <h6 className="mb-0">Arti</h6>
-                      <ReactStars
-                        count={5}
-                        size={24}
-                        value={4}
-                        edit={false}
-                        activeColor="#ffd700"
-                      />
-                    </div>
-
-                    <p className="mt-3">
-                      Lorem ipsum dolor sit amet consectetur adipisicing elit.
-                      Iure sunt perspiciatis sapiente harum, similique pariatur
-                      quam impedit! Repellat quo porro, deleniti nulla
-                      voluptatum rem nesciunt dolores error beatae! Architecto,
-                      commodi?
-                    </p>
-                  </div>
-                </div>
+                <p>{items.description}</p>
               </div>
             </div>
           </div>
