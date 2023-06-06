aa45d0ac2f2d2b401e0070669f988f7054f59a22
Art Ternava <111341549+artternava@users.noreply.github.com>
2023-06-07 01:39:38 +0200

    .

--- LejlekuXpress/Controllers/CheckOutController.cs
+++ LejlekuXpress/Controllers/CheckOutController.cs
@@ -3,7 +3,9 @@ using Microsoft.AspNetCore.Mvc;
 
 namespace LejlekuXpress.Controllers
 {
-    public class CheckOutController : Controller
+    [Route("api/[controller]")]
+    [ApiController]
+    public class CheckOutController : ControllerBase
     {
         private readonly ICheckOutService _service;
         public CheckOutController(ICheckOutService service)
