b4f6d6ea98090dd31a317c6be1c32be1227cad5e
Art Ternava <111341549+artternava@users.noreply.github.com>
2023-06-07 01:28:18 +0200

    Created Checkout Service

---
+++ LejlekuXpress/Services/CheckOutService.cs
@@ -0,0 +1,16 @@
+﻿using LejlekuXpress.Data;
+
+namespace LejlekuXpress.Services
+{
+    public class CheckOutService
+    {
+        private readonly AppDbContext _context;
+
+        public CheckOutService(AppDbContext context)
+        {
+            _context = context;
+        }
+
+
+    }
+}
