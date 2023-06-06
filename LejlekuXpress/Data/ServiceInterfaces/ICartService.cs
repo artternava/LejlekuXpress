9e6c192e3e547042926365334c8ac50f72e21c1a
ArditBeqaj <arditi.b75@gmail.com>
2023-06-06 22:43:52 +0200

    Created ICartService Service Interface

---
+++ LejlekuXpress/Data/ServiceInterfaces/ICartService.cs
@@ -0,0 +1,14 @@
+﻿using LejlekuXpress.Data.DTO;
+using LejlekuXpress.Models;
+
+namespace LejlekuXpress.Data.ServiceInterfaces
+{
+    public interface ICartService
+    {
+        Task<Cart> AddItem(CartDTO request);
+
+        Task<List<Cart>> GetByUserId(int userId);
+
+        Task DeleteItem(int id);
+    }
+}
