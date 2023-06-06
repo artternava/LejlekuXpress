7ff7296a76b1f50e1a23e1709f033337f0f7e523
ArditBeqaj <arditi.b75@gmail.com>
2023-06-06 21:16:53 +0200

    Added Cart Model and Migration

---
+++ LejlekuXpress/Migrations/20230606191501_AddedCartModel.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace LejlekuXpress.Migrations
+{
+    /// <inheritdoc />
+    public partial class AddedCartModel : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+
+        }
+    }
+}
