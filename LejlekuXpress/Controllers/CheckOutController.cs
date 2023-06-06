d5d1b79e500c1ca0481121289cf0d01f31392be8
Art Ternava <111341549+artternava@users.noreply.github.com>
2023-06-07 01:38:36 +0200

    Cretated CheckOut controller

---
+++ LejlekuXpress/Controllers/CheckOutController.cs
@@ -0,0 +1,14 @@
+﻿using LejlekuXpress.Data.ServiceInterfaces;
+using Microsoft.AspNetCore.Mvc;
+
+namespace LejlekuXpress.Controllers
+{
+    public class CheckOutController : Controller
+    {
+        private readonly ICheckOutService _service;
+        public CheckOutController(ICheckOutService service)
+        {
+            _service = service;
+        }
+    }
+}
