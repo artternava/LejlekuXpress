84ee1f3ce0a4488e4bf292ebcf81aac625e36073
ArditBeqaj <arditi.b75@gmail.com>
2023-06-07 20:31:24 +0200

    Implemented functionality to add items to the cart upon clicking the 'Add to Cart' button

--- LejlekuXpress/Front-End/src/pages/Wishlist.js
+++ LejlekuXpress/Front-End/src/pages/Wishlist.js
@@ -9,6 +9,8 @@ function Wishlist() {
   const { userId } = useAuthToken();
   const [items, setItems] = useState(null);
   const [listings, setListings] = useState(null);
+  const [addItem, setAddItem] = useState(null);
+  const [cart, setCart] = useState(null);
 
   useEffect(() => {
     fetchListings();
@@ -23,6 +25,36 @@ function Wishlist() {
       console.log(error);
     }
   };
+
+  async function handleAdd(productId) {
+    try {
+      const itemsData = {
+        UserId: parseInt(userId, 10),
+        ProductId: productId,
+      }
+      console.log(itemsData);
+      setAddItem(itemsData)
+      await axios.post('http://localhost:39450/api/Cart/add', addItem)
+      .then((response) => {
+        window.alert('add successful');
+      });
+      fetchCart(); 
+    } catch (error) {
+      console.error('Error adding items:', error);
+    }
+  }
+
+  async function fetchCart() {
+    try {
+      const response = await axios.get(`http://localhost:39450/api/Cart/getbyuserid?userID=${userId}`);
+      if (response.status === 200) {
+        const itemsResponse = response.data;
+        setCart(itemsResponse);
+        }
+      } catch (error) {
+        console.error(error);
+      }
+    }
 
   async function fetchWishlist() {
     try {
@@ -137,7 +169,7 @@ if (!items) {
                    <div className="col-md-12 col-lg-3 col-xl-3 mb-4 mb-lg-0 d-flex ">
                      <div className="bg-image hover-zoom ripple rounded ripple-surface ">
                        <Link to={`/product/${item.productId}`} className="input-group-text p-3 justify-content-center" id="basic-addon2">
-                         <img className="" src={`data:image/${getImageExtension(getimage(item.productId))};base64,${getimage(item.productId)}`} alt="Product" style={{ width: "60%", aspectRatio: "1/1" }} />
+                         <img className="" src={`data:image/${getImageExtension(getimage(item.productId))};base64,${getimage(item.productId)}`} alt="Product" style={{ width: "70%", height: "auto", objectFit: "contain" }} />
                        </Link>
                        <a href="#!">
                          <div className="hover-overlay">
@@ -167,7 +199,9 @@ if (!items) {
                      </div>
                      <h6 className="text-success">Shipping: ${getShippingPrice(item.productId)}</h6>
                      <div className="d-flex flex-column mt-4">
-                       <button className="btn btn-primary btn-sm" type="button">Add to Cart</button>
+                       <button className="btn btn-primary btn-sm" type="button" onClick={() => handleAdd(item.id)}>
+                        Add to Cart
+                        </button>
                        <button className="btn btn-outline-danger btn-sm mt-2" type="button" onClick={() => handleDelete(item.id)}>
                          Delete
                        </button>
