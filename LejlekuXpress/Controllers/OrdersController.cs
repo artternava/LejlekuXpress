af659ab62acd3731555bb591a108b39c984a1533
andiferataj <andi.fr33@gmail.com>
2023-06-11 22:30:37 +0200

    Created Orders Controller and injected service

---
+++ LejlekuXpress/Controllers/OrdersController.cs
@@ -0,0 +1,18 @@
+﻿using LejlekuXpress.Data.ServiceInterfaces;
+using Microsoft.AspNetCore.Mvc;
+
+namespace LejlekuXpress.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class OrdersController : ControllerBase
+    {
+        private readonly IOrdersService _service;
+
+        public OrdersController(IOrdersService service)
+        {
+            _service = service;
+        }
+
+    }
+}
