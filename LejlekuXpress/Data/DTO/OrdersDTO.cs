fda1866ca5be1c48ed28be5c64bdeb0c9abcb985
andiferataj <andi.fr33@gmail.com>
2023-06-11 22:15:27 +0200

    Created OrdersDTO

---
+++ LejlekuXpress/Data/DTO/OrdersDTO.cs
@@ -0,0 +1,9 @@
+﻿namespace LejlekuXpress.Data.DTO
+{
+    public class OrdersDTO
+    {
+        public int UserId { get; set; }
+        public int ProductId { get; set; }
+        public int Quantity { get; set; }
+    }
+}
