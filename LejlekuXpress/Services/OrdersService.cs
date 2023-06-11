2b1c2fb16ffbff6b704d80f3b4b6cae0d1e6853b
andiferataj <andi.fr33@gmail.com>
2023-06-11 22:14:07 +0200

    Created Orders service and injected dbContext

---
+++ LejlekuXpress/Services/OrdersService.cs
@@ -0,0 +1,14 @@
+﻿using LejlekuXpress.Data;
+
+namespace LejlekuXpress.Services
+{
+    public class OrdersService
+    {
+        private readonly AppDbContext _context;
+
+        public OrdersService(AppDbContext context)
+        {
+            _context = context;
+        }
+    }
+}
