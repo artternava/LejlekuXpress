5a84e4eed43322d16cd972946f13e7d8458b022c
andiferataj <andi.fr33@gmail.com>
2023-06-11 22:25:35 +0200

    added add items method in orders service interface

--- LejlekuXpress/Data/ServiceInterfaces/IOrdersService.cs
+++ LejlekuXpress/Data/ServiceInterfaces/IOrdersService.cs
@@ -1,6 +1,10 @@
-﻿namespace LejlekuXpress.Data.ServiceInterfaces
+﻿using LejlekuXpress.Data.DTO;
+using LejlekuXpress.Models;
+
+namespace LejlekuXpress.Data.ServiceInterfaces
 {
     public interface IOrdersService
     {
+        Task<Orders> AddItem(OrdersDTO request);
     }
 }
