88fb8373f69305c077bb4b0a98fb4ecff5c3f51a
artternava <artternava28@gmail.com>
2023-06-15 00:13:38 +0200

    migration fix

--- LejlekuXpress/Migrations/20230614215433_shabllon.cs
+++ LejlekuXpress/Migrations/20230614215433_shabllon.cs
@@ -10,90 +10,48 @@ namespace LejlekuXpress.Migrations
         /// <inheritdoc />
         protected override void Up(MigrationBuilder migrationBuilder)
         {
-            migrationBuilder.DropTable(
-                name: "Lojtari");
+            //migrationBuilder.DropTable(
+            //    name: "Lojtari");
 
-            migrationBuilder.DropTable(
-                name: "Skuadra");
+            //migrationBuilder.DropTable(
+            //    name: "Skuadra");
 
-            migrationBuilder.CreateTable(
-                name: "Shabllon",
-                columns: table => new
-                {
-                    Id = table.Column<int>(type: "int", nullable: false)
-                        .Annotation("SqlServer:Identity", "1, 1"),
-                    FirstName = table.Column<string>(type: "nvarchar(max)", nullable: false),
-                    LastName = table.Column<string>(type: "nvarchar(max)", nullable: false),
-                    Email = table.Column<string>(type: "nvarchar(max)", nullable: false),
-                    PhoneNumber = table.Column<string>(type: "nvarchar(max)", nullable: true),
-                    RoleId = table.Column<int>(type: "int", nullable: false)
-                },
-                constraints: table =>
-                {
-                    table.PrimaryKey("PK_Shabllon", x => x.Id);
-                    table.ForeignKey(
-                        name: "FK_Shabllon_Roles_RoleId",
-                        column: x => x.RoleId,
-                        principalTable: "Roles",
-                        principalColumn: "Id",
-                        onDelete: ReferentialAction.Cascade);
-                });
+            //migrationBuilder.CreateTable(
+            //    name: "Shabllon",
+            //    columns: table => new
+            //    {
+            //        Id = table.Column<int>(type: "int", nullable: false)
+            //            .Annotation("SqlServer:Identity", "1, 1"),
+            //        FirstName = table.Column<string>(type: "nvarchar(max)", nullable: false),
+            //        LastName = table.Column<string>(type: "nvarchar(max)", nullable: false),
+            //        Email = table.Column<string>(type: "nvarchar(max)", nullable: false),
+            //        PhoneNumber = table.Column<string>(type: "nvarchar(max)", nullable: true),
+            //        RoleId = table.Column<int>(type: "int", nullable: false)
+            //    },
+            //    constraints: table =>
+            //    {
+            //        table.PrimaryKey("PK_Shabllon", x => x.Id);
+            //        table.ForeignKey(
+            //            name: "FK_Shabllon_Roles_RoleId",
+            //            column: x => x.RoleId,
+            //            principalTable: "Roles",
+            //            principalColumn: "Id",
+            //            onDelete: ReferentialAction.Cascade);
+            //    });
 
-            migrationBuilder.CreateIndex(
-                name: "IX_Shabllon_RoleId",
-                table: "Shabllon",
-                column: "RoleId");
+            //migrationBuilder.CreateIndex(
+            //    name: "IX_Shabllon_RoleId",
+            //    table: "Shabllon",
+            //    column: "RoleId");
         }
 
         /// <inheritdoc />
         protected override void Down(MigrationBuilder migrationBuilder)
         {
-            migrationBuilder.DropTable(
-                name: "Shabllon");
+            //migrationBuilder.DropTable(
+            //    name: "Shabllon");
 
-            migrationBuilder.CreateTable(
-                name: "Skuadra",
-                columns: table => new
-                {
-                    Id = table.Column<int>(type: "int", nullable: false)
-                        .Annotation("SqlServer:Identity", "1, 1"),
-                    Name = table.Column<string>(type: "nvarchar(max)", nullable: false),
-                    NrOfPlayers = table.Column<int>(type: "int", nullable: false),
-                    Stadium = table.Column<string>(type: "nvarchar(max)", nullable: false)
-                },
-                constraints: table =>
-                {
-                    table.PrimaryKey("PK_Skuadra", x => x.Id);
-                });
-
-            migrationBuilder.CreateTable(
-                name: "Lojtari",
-                columns: table => new
-                {
-                    Id = table.Column<int>(type: "int", nullable: false)
-                        .Annotation("SqlServer:Identity", "1, 1"),
-                    SkuadraId = table.Column<int>(type: "int", nullable: false),
-                    FirstName = table.Column<string>(type: "nvarchar(max)", nullable: false),
-                    JerseyNumber = table.Column<int>(type: "int", nullable: false),
-                    LastName = table.Column<string>(type: "nvarchar(max)", nullable: false),
-                    Position = table.Column<string>(type: "nvarchar(max)", nullable: false),
-                    Salary = table.Column<decimal>(type: "decimal(18,2)", nullable: false)
-                },
-                constraints: table =>
-                {
-                    table.PrimaryKey("PK_Lojtari", x => x.Id);
-                    table.ForeignKey(
-                        name: "FK_Lojtari_Skuadra_SkuadraId",
-                        column: x => x.SkuadraId,
-                        principalTable: "Skuadra",
-                        principalColumn: "Id",
-                        onDelete: ReferentialAction.Cascade);
-                });
-
-            migrationBuilder.CreateIndex(
-                name: "IX_Lojtari_SkuadraId",
-                table: "Lojtari",
-                column: "SkuadraId");
+            
         }
     }
 }
